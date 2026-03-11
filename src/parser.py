3f6dc091ed4dd65a913717abb5af7ed69b8bb2ed
GPahuja67 <gaurikapahuja05@gmail.com>
2026-03-11 17:03:39 +0530

    Add order model

---
+++ src/parser.py
@@ -0,0 +1,18 @@
+from src.order import Order
+import time
+
+def parse_line(line):
+
+    parts = line.strip().split()
+
+    if parts[0] == "CANCEL":
+        return ("CANCEL", parts[1])
+
+    order_id = parts[0]
+    side = parts[1]
+    price = float(parts[2])
+    qty = int(parts[3])
+
+    order = Order(order_id, side, price, qty, time.time())
+
+    return ("ORDER", order)
