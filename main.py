2f8e79c4f28c8d25fecb558f4b9963be68fc3dd3
GPahuja67 <gaurikapahuja05@gmail.com>
2026-03-11 17:18:49 +0530

    Add CLI entry point for running the order book engine

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from src.engine import run_engine
 if __name__ == "__main__":
 
     if len(sys.argv) < 2:
+
         print("Usage: python main.py <input_file>")
         sys.exit(1)
 
-    input_file = sys.argv[1]
-
-    run_engine(input_file)
+    run_engine(sys.argv[1])
